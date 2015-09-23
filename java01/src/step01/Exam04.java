9016d9efbf97823b9458c773957521c20ab50394
BitCamp <BitCamp@BitCamp-PC>
2015-09-23 10:54:43 +0900

    .

--- java01/src/step01/Exam04.java
+++ java01/src/step01/Exam04.java
@@ -7,6 +7,19 @@ package step01;
 public class Exam04/*프로그램명*/ {
   //Program Entry : 프로그램 시작점
   public static void main(String[] args) {
-
+    System.out.println("Hello!");
+    System.out.println();
+    
+    System.out.println(10);
+    System.out.println(-20);
+    System.out.println();
+    
+    System.out.println(3.14159);
+    System.out.println(-27.56);
+    System.out.println();
+    
+    System.out.println(true);
+    System.out.println(false);
+    System.out.println();
   }
 }
