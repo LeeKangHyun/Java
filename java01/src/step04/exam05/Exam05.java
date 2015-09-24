4f71f1b119e503582695122cb86b37bba8b19d9d
BitCamp <BitCamp@BitCamp-PC>
2015-09-24 11:01:48 +0900

    .

--- java01/src/step04/exam05/Exam05.java
+++ java01/src/step04/exam05/Exam05.java
@@ -1,25 +1,23 @@
 /*
- * 주제 : 기능 정의4
- * - Function = Method
+ * 주제 : 도구 생성
+ * - class
  * - 문법
- *   작업결과값의종류 기능명(작업할때사용할값1, 갑2, 값3....) {
- *     명령어..
+ *   class 도구명 {
+ *     //변수 선언
+ *     //메소드 정의
  *   }
  */
 package step04.exam05;
 
-public class Exam05 {
-
-  //1) 메소드 정의
-  //return: 있음
-  //parameter: 있음
+class A {
   static String Hello(String name) {
     return "Hello! " + name + "씨?!";
-  }
+  }  
+}
 
+public class Exam05 {
   public static void main(String[] args) {
-    String str = Hello("홍길동");  //메소드 호출했지만 return값을 사용하지 않는 예
+    String str = A.Hello("홍길동");  //메소드 호출했지만 return값을 사용하지 않는 예
     System.out.println(str);
   }
-
 }
