02a953c30c4af857995574319084f1da3bba79f9
BitCamp <BitCamp@BitCamp-PC>
2015-09-24 15:40:41 +0900

    .

--- java02/src/step01/Exam02.java
+++ java02/src/step01/Exam02.java
@@ -6,6 +6,6 @@ package step01;
 
 public class Exam02 {
   public static void main(String[] args) {
-    System.out.println(Integer.toString(10, 2));
+    System.out.println(Integer.toString(3, 2));
   }
 }
