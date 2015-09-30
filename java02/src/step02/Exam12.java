a6a97b6831b92a81f8213d0bd1340d845056e761
leekanghyun <leekanghyun@218.38.137.8>
2015-09-30 12:13:30 +0900

    .

--- java02/src/step02/Exam12.java
+++ java02/src/step02/Exam12.java
@@ -1,67 +1,21 @@
 /*
- * 주제: 논리 연산자 - &&(and), ||(or), &, |
+ * 주제: 조건 연산자: (논리값) ? 문장1:문장2
  */
 package step02;
 
 public class Exam12 {
 
   public static void main(String[] args) {
-    //AND
-    System.out.println(true && true);
-    System.out.println(true && false);
-    System.out.println(false && true);
-    System.out.println(false && false);
-    System.out.println();
+    int age = 30;
     
-    //OR
-    System.out.println(true || true);
-    System.out.println(true || false);
-    System.out.println(false || true);
-    System.out.println(false || false);
-    System.out.println();
+    // 조건문
+    if (age >= 19)
+      System.out.println("성인입니다.");
+    else
+      System.out.println("미성년자입니다.");
     
-    //AND: &&, ||와 &, |의 차이점
-    //1) &&, || : 앞의 값이 무엇이냐에 따라 뒤의 명령문을 실행할지 결정한다.
-    boolean b = false;
-    boolean result = true && (b = true);  
-    // 앞의 값이 true라면, 뒤에 값에 따라 결과가 달라지기 때문에
-    // 반드시 뒤의 명령문을 실행한다.
-    System.out.printf("%b, %b\n", result, b);
-    
-    b = false;
-    // 앞의 값이 false이기 때문에 결과는 무조건 false이다.
-    // 따라서 뒤의 명령문을 실행할 필요가 없다.
-    result = false && (b = true);
-    System.out.printf("%b, %b\n", result, b);
-    
-    b = false;
-    //  앞의 값이 true이면 뒤의 값에 상관없이 결과는 true이다.
-    //  따라서 뒤의 명령문을 실행할 필요가 없다.
-    result = true || (b = true);
-    System.out.printf("%b, %b\n", result, b);
-    
-    b = false;
-    //  앞의 값이 false이기 떄문에, 뒤의 명령문을 실행해 봐야지만 결과를 알 수 있다.
-    result = false || (b = true);
-    System.out.printf("%b, %b\n", result, b);
-    System.out.println();
-    
-    //1) &, | : 앞의 값과 상관없이 무조건 뒤의 명령문을 실행한다.
-    b = false;
-    result = true & (b = true);  
-    System.out.printf("%b, %b\n", result, b);
-    
-    b = false;
-    result = false & (b = true);
-    System.out.printf("%b, %b\n", result, b);
-    
-    b = false;
-    result = true | (b = true);
-    System.out.printf("%b, %b\n", result, b);
-    
-    b = false;
-    result = false | (b = true);
-    System.out.printf("%b, %b\n", result, b);
+    // 조건 연산자
+    System.out.println((age >= 19) ? "성인입니다." : "미성년자입니다.");
   }
 }
 
