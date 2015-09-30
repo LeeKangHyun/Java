dcc154787db5974b1cdcb0225a0fb1cd7139cda7
leekanghyun <leekanghyun@218.38.137.8>
2015-09-30 19:42:08 +0900

    .

--- java01/src/Calc01.java
+++ java01/src/Calc01.java
@@ -3,30 +3,28 @@ import java.util.Scanner;
 
 public class Calc01 {
 
-  class 
-  public static void main(String[] args) {
-    InputStream ks = System.in;
-    Scanner sc = new Scanner(ks);
+//    InputStream ks = System.in;
+//    Scanner sc = new Scanner(ks);
+//    
+//    int result;
+//    
+//    System.out.println("계산기 프로그램.");
+//    System.out.println("Input Number> ");
+//    int n1 = sc.nextInt();
+//    int n2 = sc.nextInt();
+//    
+//    System.out.println("1. + \n2. - \n3. * \n4. / \n5. Exit");
+//    System.out.println("Input Menu> ");
+//    int m = sc.nextInt();
+//    
+//    if (m == 1)
+//      result = n1 + n2;
+//    else if (m == 2)
+//      result = n1 - n2;
+//    else if
+//    
+//    System.out.println(n1 +" "+ n2);
     
-    int result;
-    
-    System.out.println("계산기 프로그램.");
-    System.out.println("Input Number> ");
-    int n1 = sc.nextInt();
-    int n2 = sc.nextInt();
-    
-    System.out.println("1. + \n2. - \n3. * \n4. / \n5. Exit");
-    System.out.println("Input Menu> ");
-    int m = sc.nextInt();
-    
-    if (m == 1)
-      result = n1 + n2;
-    else if (m == 2)
-      result = n1 - n2;
-    else if
-    
-    System.out.println(n1 +" "+ n2);
-    
-  }
+//  }
 
 }
