f6863af033301c99ec8a2d42c59a7a4d476dbb77
Administrator <Administrator@kangto-PC>
2015-10-04 01:17:11 +0900

    .

---
+++ java02/src/Email01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Email01 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Email Input> ");
+    String str = sc.nextLine();
+    sc.close();
+
+    String RegEx = "^[a-zA-Z0-9]+" // 모든단어 1글자이상으로 시작
+        + "@[a-zA-Z0-9]+" // @뒤에 1글자 이상
+        + "(.[a-zA-z0-9]+)"  // .뒤에 1글자 이상
+        + "(.[a-zA-z0-9])*";  // a@a.a 뒤에 서버용 이메일 .이 있어도 되고 없어도 되고
+    //              ^\w         +@\w         +(.\w         +)       
+    boolean tf;
+
+    tf = Pattern.matches(RegEx, str);
+
+    if (tf)
+      System.out.println(str + " is Email.");
+    else
+      System.out.println(str + " is not Email");
+  }
+}
