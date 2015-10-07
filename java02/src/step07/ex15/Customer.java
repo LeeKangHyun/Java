f8815cbcaece200df94e39fcc176f1e0a761f05f
leekanghyun <leekanghyun@localhost>
2015-10-07 15:14:45 +0900

    .

---
+++ java02/src/step07/ex15/Customer.java
@@ -0,0 +1,9 @@
+package step07.ex15;
+
+public class Customer {
+  String  name;
+  boolean gender;
+  float   height;
+  float   weight;
+  float   fatRate;
+}
---
+++ java02/src/step07/ex15/Test.java
@@ -0,0 +1,10 @@
+package step07.ex15;
+
+public class Test {
+
+  public static void main(String[] args) {
+    
+
+  }
+
+}
