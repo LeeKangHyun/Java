128f4ef35ecd2da6f6146bc8415fb5ca57a0ad6a
leekanghyun <leekanghyun@localhost>
2015-10-13 20:08:36 +0900

    .

--- java02/src/step09/v09/StudentControl.java
+++ java02/src/step09/v09/StudentControl.java
@@ -1,20 +1,20 @@
 package step09.v09;
 
-import java.sql.Date;
 import java.util.Scanner;
 
 public class StudentControl {
   Scanner scanner;
-  Project[] projects = new Project[1000];
+  Student[] students = new Student[1000];
   int pos; // 저장 위치
   
   public StudentControl(Scanner scanner) {
     this.scanner = scanner;
   }
+  
   public void service() {
     String command = null;
     do {
-      System.out.print("명령> ");
+      System.out.print("학생관리> ");
       command = scanner.nextLine(); 
       switch (command) {
       case "list":
@@ -25,12 +25,12 @@ public class StudentControl {
         break;
       case "delete":
         doDelete();
-        break;  
-      case "menu":
+        break; 
+      case "main":
         return;
       case "help":
         doHelp();
-        break;
+        break;  
       default:
         System.out.println("해당 명령을 지원하지 않습니다.");
       }
@@ -38,41 +38,38 @@ public class StudentControl {
   }
   
   private void doList() {
-    System.out.printf("%-3s %-20s %-10s %-10s %-40s\n", 
-        "No", "Title", "Start", "End", "Members");
-
     for (int i = 0; i < pos; i++) {
-      if (projects[i] == null) // 배열의 항목이 null인 경우, 다음 항목으로 바로 이동.
+      if (students[i] == null) // 배열의 항목이 null인 경우, 다음 항목으로 바로 이동.
         continue;
-      System.out.printf("% 3d %-20s %3$tY-%3$tm-%3$td %4$s %5$-40s\n", 
+      System.out.printf("%d %s %s %s %s\n", 
           i, 
-          projects[i].title,
-          projects[i].startDate,
-          projects[i].endDate,
-          projects[i].member);
+          students[i].name,
+          students[i].email,
+          students[i].tel,
+          students[i].cid);
     }
   }
   
   private void doAdd() {
-    Project project = new Project();
+    Student student = new Student();
     
-    System.out.print("프로젝트명? ");
-    project.setTitle(scanner.nextLine());
+    System.out.print("이름? ");
+    student.setName(scanner.nextLine());
     
-    System.out.print("시작일? ");
-    project.setStartDate(Date.valueOf(scanner.nextLine()));
+    System.out.print("이메일? ");
+    student.setEmail(scanner.nextLine());
     
-    System.out.print("종료일? ");
-    project.setEndDate(Date.valueOf(scanner.nextLine()));
+    System.out.print("전화? ");
+    student.setTel(scanner.nextLine());
 
-    System.out.print("멤버? ");
-    project.setMember(scanner.nextLine());
+    System.out.print("기수? ");
+    student.setCid(scanner.nextLine());
     
     System.out.print("정말 저장하시겠습니까?(y/n)");
     String yesno = scanner.nextLine();
     if (yesno.toLowerCase().equals("y")) {
-      if (pos < projects.length) { 
-        projects[pos++] = project;
+      if (pos < students.length) { 
+        students[pos++] = student;
         System.out.println("저장되었습니다.");
       } else {
         System.out.println("저장소가 모두 찼습니다.\n저장할 수 없습니다!");
@@ -83,18 +80,18 @@ public class StudentControl {
   }
   
   private void doDelete() {
-    System.out.print("프로젝트 번호? ");
+    System.out.print("회원 번호? ");
     int no = Integer.parseInt(scanner.nextLine());
     
     System.out.print("정말 삭제하시겠습니까?(y/n)");
     String yesno = scanner.nextLine();
     if (yesno.toLowerCase().equals("y")) {
       if (no > -1 && no < pos) { 
-        if (projects[no] == null) {
+        if (students[no] == null) {
           System.out.println("이미 삭제된 항목입니다.");
         } else {
           for (int i = no; i < (pos - 1); i++) {
-            projects[i] = projects[i + 1];
+            students[i] = students[i + 1];
           }
           pos--;
           System.out.println("삭제하였습니다.");
@@ -106,15 +103,15 @@ public class StudentControl {
       System.out.println("취소하였습니다.");
     }
   }
+  
   private void doHelp() {
     System.out.println("[사용법]");
     System.out.println("명령");
     System.out.println();
     System.out.println("[명령]");
-    System.out.println("menu         메뉴 목록을 리턴한다.");
-    System.out.println("menu 번호     해당 번호를 프로젝트를 추가한다.");
-    System.out.println("help         도움말을 출력한다.");
-    System.out.println("quit         프로그램을 종료한다.");
+    System.out.println("list         학생 목록을 리턴한다.");
+    System.out.println("add          학생을 추가한다.");
+    System.out.println("delete       학생을 삭제한다. ");
+    System.out.println("main         메인으로 이동한다.");
   }
-
 }
