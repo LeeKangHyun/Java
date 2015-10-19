92f535ae14d2c28fa96220a62d605dff0a605ccf
leekanghyun <leekanghyun@218.38.137.8>
2015-10-19 11:57:04 +0900

    .

---
+++ java02/src/step13/ex03/Test01.java
@@ -0,0 +1,17 @@
+package step13.ex03;
+
+public class Test01 {
+  public class Student {
+    String name;
+    String tel;
+    String location;
+    int workstate;
+    int subject;
+  }
+
+  public static void main(String[] args) {
+    Student s1 = new Student();
+    s1.name = "홍길동";
+    s1.tel = "111-1111";
+  }
+}
