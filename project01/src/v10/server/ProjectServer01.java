7dccc716635cbd64598d0cd1a0dc21fc20c2f106
leekanghyun <leekanghyun@192.168.0.55>
2015-10-30 10:30:37 +0900

    .

--- project01/src/v10/server/ProjectServer01.java
+++ project01/src/v10/server/ProjectServer01.java
@@ -4,35 +4,33 @@
 package v10.server;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
 
-import v10.server.context.PropertyFileApplicationContext;
 import v10.server.servlet.Servlet;
 
-public class ProjectServer {
-  PropertyFileApplicationContext context;
+public class ProjectServer01 {
+  HashMap<String,Object> objMap = new HashMap<String,Object>();
 
   class RequestHandler implements Runnable {
     Socket socket;
-
     public RequestHandler(Socket socket) {
       this.socket = socket;
     }
-
     @Override
     public void run() {
       System.out.println("클라이언트 요청 처리...");
       try (
-          BufferedReader in = new BufferedReader(
-              new InputStreamReader(
-                  socket.getInputStream()));
-          PrintStream out = new PrintStream(
-              socket.getOutputStream());
+          BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+          PrintStream out = new PrintStream(socket.getOutputStream());
           )
       {
         String message = in.readLine();
@@ -45,7 +43,7 @@ public class ProjectServer {
 
         extractParamDataFromMessage(params, message);
 
-        Object servlet = context.getBean(command);
+        Object servlet = objMap.get(command);
 
         if (servlet != null && servlet instanceof Servlet)
           ((Servlet)servlet).service(params);
@@ -63,14 +61,57 @@ public class ProjectServer {
     }
   }
 
-  public ProjectServer() throws Exception {
-    context = new PropertyFileApplicationContext(
-        "./src/v10/server/application-context.properties");
+  public ProjectServer01() throws Exception {
+    creadteObjects();
+    injectDepencies();
+  }
+
+  private void injectDepencies() throws Exception {
+    Object[] objList = objMap.values().toArray();
+    Object dependency = null;
+
+    for (Object obj : objList) {
+      for (Method m : obj.getClass().getMethods()) {
+        if (!isSetter(m)) continue;
+        dependency = findObjectByType(m.getParameterTypes()[0]); 
+        if (dependency == null ) continue;
+        m.invoke(obj, dependency);
+      }
+    }
+  }
+
+  private boolean isSetter(Method m) {
+    if (m.getName().startsWith("set")
+        && m.getParameterTypes().length ==1)
+      return true;
+    return false;
+  }
+
+  private Object findObjectByType(Class<?> type) {
+    Object[] objList = objMap.values().toArray();
+
+    for (Object obj : objList) {
+      if (type.isInstance(obj))
+        return obj;
+    }
+    return null;
+  }
+
+  private void creadteObjects() throws Exception {
+    Properties props = new Properties();
+    props.load(
+        new FileReader("./src/v10/server/application-context.properties"));
+
+    Class<?> clazz = null;
+    for (Entry<Object,Object> entry : props.entrySet()) {
+      clazz = Class.forName((String)entry.getValue());
+      objMap.put((String)entry.getKey(), clazz.newInstance());
+    }
   }
 
   public static void main(String[] args) {
     try {
-      ProjectServer server = new ProjectServer();
+      ProjectServer01 server = new ProjectServer01();
       server.execute();
     } catch (Exception e) {
       e.printStackTrace();
