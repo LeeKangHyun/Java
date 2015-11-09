bca8e1320fd78cfc14e50eb47cf0ead106135ddd
leekanghyun <leekanghyun@localhost>
2015-11-09 19:10:05 +0900

    .

---
+++ spring01/src/main/java/step01/Test02.java
@@ -0,0 +1,17 @@
+package step01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test02 {
+  public static void main(String[] args) {
+    // 스프링 설정 파일을 읽고 인스턴스를 준비하라.
+    ApplicationContext context = new ClassPathXmlApplicationContext(
+        "step01/application-context02.xml");
+    
+    Computer c1 = (Computer)context.getBean("c1");
+    System.out.println(c1);
+    
+    Computer c2 = (Computer)context.getBean("c2");
+  }
+}
