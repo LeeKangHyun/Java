1c4eabf1dfe6d1f3de694ceba217053ea04dbf11
leekanghyun <leekanghyun@localhost>
2015-11-11 12:08:42 +0900

    .

---
+++ spring01/src/main/java/step05/HDD.java
@@ -0,0 +1,29 @@
+package step05;
+
+public class HDD {
+  protected int capacity;
+  protected int rpm;
+  
+  @Override
+  public String toString() {
+    return "HDD [capacity=" + capacity + ", rpm=" + rpm + "]";
+  }
+  
+  public int getCapacity() {
+    return capacity;
+  }
+  
+  public void setCapacity(int capacity) {
+    this.capacity = capacity;
+  }
+  
+  public int getRpm() {
+    return rpm;
+  }
+  
+  public void setRpm(int rpm) {
+    this.rpm = rpm;
+  }
+  
+  
+}
