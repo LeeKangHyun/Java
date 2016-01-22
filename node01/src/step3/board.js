ef103af7bfb16e47f41979899afcd4a4770c5277
Kanghyun Lee <rkdgusdl92@gmail.com>
2016-01-22 16:18:42 +0900

    .

--- node01/src/step3/board.js
+++ node01/src/step3/board.js
@@ -1,8 +1,9 @@
-// DB 커넥션풀 적용하기
+//DB 커넥션풀 적용하기
 var http = require('http');
 var mysql = require('mysql');
 var url = require('url');
 var dateFormat = require('dateformat');
+var qs = require('querystring');
 
 var pool = mysql.createPool({
     connectionLimit : 10,
@@ -12,7 +13,7 @@ var pool = mysql.createPool({
     database : 'java76db'
 });
 
-// 커넥션풀에 새 커넥션이 생성되면 connection 이벤트가 발생한다.
+//커넥션풀에 새 커넥션이 생성되면 connection 이벤트가 발생한다.
 pool.on('connection', function() {
     console.log("커넥션 객체가 생성되었음.");
 });
@@ -66,60 +67,89 @@ handlerMap["/board/detail.do"] = function(request, response) {
     pool.query(
             'select bno, title, content, views, cre_dt from board where bno=?', 
             [urlInfo.query.no], 
-        function(err, rows, fields) {
-            if (err) throw err;
-            response.write("<!DOCTYPE html>\n");
-            response.write("<html>\n");
-            response.write("<head>\n");
-            response.write("<meta charset=\'UTF-8\'>\n");
-            response.write("<title>게시판</title>\n");
-            response.write("</head>\n");
-            response.write("<body>\n");
-            response.write("<h1>게시물 목록</h1>\n");
-
-            response.write("<table border='1'>\n");
-
-            response.write("<tr>\n");
-            response.write("  <th>번호</th>\n");
-            response.write("  <td><input type='text' name='no' value='" + rows[0].bno + "'></td>\n");
-            response.write("</tr>\n");
-            response.write("<tr>\n");
-            response.write("  <th>제목</th>\n");
-            response.write("  <td><input type='text' name='title' value='" + rows[0].title + "'></td>\n");
-            response.write("</tr>\n");
-            response.write("<tr>\n");
-            response.write("  <th>내용</th>\n");
-            response.write("  <td><textarea rows='5' cols='50'>" + rows[0].content + "</textarea></td>\n");
-            response.write("</tr>\n");
-            response.write("<tr>\n");
-            response.write("  <th>조회수</th>\n");
-            response.write("  <td>" + rows[0].views + "</td>\n");
-            response.write("</tr>\n");
-            response.write("<tr>\n");
-            response.write("  <th>등록일</th>\n");
-            response.write("  <td>" + dateFormat(rows[0].cre_dt, "yyyy-m-dd") + "</td>\n");
-            response.write("</tr>\n");
-
-            response.write("</table>\n");
-            response.write("<p><a href='delete.do?no=" + rows[0].bno + "'>삭제</a></p>\n");
-            response.write("</body>\n");
-            response.write("</html>\n");
-            response.end();
-        }
+            function(err, rows, fields) {
+                if (err) throw err;
+                response.write("<!DOCTYPE html>\n");
+                response.write("<html>\n");
+                response.write("<head>\n");
+                response.write("<meta charset=\'UTF-8\'>\n");
+                response.write("<title>게시판</title>\n");
+                response.write("</head>\n");
+                response.write("<body>\n");
+                response.write("<h1>게시물 상세정보</h1>\n");
+                response.write("<form action='update.do' method='POST'>\n")
+                response.write("<table border='1'>\n");
+                response.write("<tr>\n");
+                response.write("  <th>번호</th>\n");
+                response.write("  <td><input type='text' name='no' value='" + rows[0].bno + "'></td>\n");
+                response.write("</tr>\n");
+                response.write("<tr>\n");
+                response.write("  <th>제목</th>\n");
+                response.write("  <td><input type='text' name='title' value='" + rows[0].title + "'></td>\n");
+                response.write("</tr>\n");
+                response.write("<tr>\n");
+                response.write("  <th>내용</th>\n");
+                response.write("  <td><textarea name='content' rows='5' cols='50'>" + rows[0].content + "</textarea></td>\n");
+                response.write("</tr>\n");
+                response.write("<tr>\n");
+                response.write("  <th>조회수</th>\n");
+                response.write("  <td>" + rows[0].views + "</td>\n");
+                response.write("</tr>\n");
+                response.write("<tr>\n");
+                response.write("  <th>등록일</th>\n");
+                response.write("  <td>" + dateFormat(rows[0].cre_dt, "yyyy-m-dd") + "</td>\n");
+                response.write("</tr>\n");
+                response.write("</table>\n");
+                response.write("<p>"  
+                        + "<input type='submit' value='변경'>"
+                        + "<a href='delete.do?no=" + rows[0].bno + "'>삭제</a>" 
+                        + "</p>\n");
+                response.write("</form>\n");
+                response.write("</body>\n");
+                response.write("</html>\n");
+                response.end();
+            }
     );
 };
 
 handlerMap["/board/delete.do"] = function(request, response) {
     var urlInfo = url.parse(request.url, true);
     pool.query('delete from board where bno=?', [urlInfo.query.no],
-        function(err, rows, fields) {
-          if (err) throw err;
-          response.writeHead(302, {
-              'Location': '/board/list.do'
-          });
-          response.end();
+            function(err, rows, fields) {
+        if (err) throw err;
+        response.writeHead(302, {
+            'Location': '/board/list.do'
+        });
+        response.end();
     });
-}
+};
+
+handlerMap["/board/update.do"] = function(request, response) {
+    var urlInfo = url.parse(request.url, true);
+    
+    var message = '';
+    // 'data' 이벤트 => POST 요청 시, 
+    //                  message-body 영역의 데이터를 읽을 때 발생하는 이벤
+    request.on('data', function(data) {
+        message += data;
+    });
+
+    // 'end' 이벤트 => POST 요청 시, 
+    //                  message-body 영역의 데이터를 읽을 때 발생하는 이벤
+    request.on('end', function() {
+        var params = qs.parse(message);
+        pool.query(
+                'update board set title=?, content=? where bno=?', 
+                [params.title, params.content, params.no],
+                function(err, rows, fields) {
+                    if (err) throw err;
+                    response.writeHead(302, {
+                        'Location': '/board/list.do'
+                    });
+                    response.end();
+                });
+    });
+};
 
 function notSupport(request, response) {
     response.writeHead(200, {
@@ -140,6 +170,9 @@ function notSupport(request, response) {
 
 var httpServer = http.createServer(function(request, response) {
     var urlInfo = url.parse(request.url, true);
+    response.writeHead(200, {
+        'Content-Type': 'text/html; charset=UTF-8'
+    });
 
     var handler = handlerMap[urlInfo.pathname];
 
